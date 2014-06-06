f5329e46f837da82c8a0b9c2519fa56329a02b75
RSid <sm1th303@gmail.com>
2014-06-06 13:11:37 -0400

    adding contacts enabled

--- app.rb
+++ app.rb
@@ -14,8 +14,15 @@ require_relative 'models/contact'
 
 get '/' do
   @page = params[:page].to_i
+  @query = params[:query]
+
+  if @query
+    #@results = Contact.where("contact.name = #{@query}")
+    #binding.pry
+  end
 
   @contacts = Contact.limit(3).offset(3 * @page)
+
 
   erb :index
 end
@@ -24,4 +31,18 @@ get '/contacts/:id' do
   @contact =  Contact.find(params[:id])
 
   erb :show
+end
+
+get '/add' do
+
+  erb :add
+end
+
+post '/add' do
+  first_name = params['add-first']
+  last_name = params['add-last']
+  phone_number = params['add-number']
+  Contact.find_or_create_by(first_name: first_name,last_name: last_name,phone_number: phone_number)
+
+  redirect '/'
 end
